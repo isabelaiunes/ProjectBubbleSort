717ce4ced4054e361133ca56b57bad25d11fa86f
Isabela Iunes <iunesisabela.cs@gmail.com>
2024-10-25 09:32:02 -0300

    master

--- BubbleSort.py
+++ BubbleSort.py
@@ -9,3 +9,8 @@
 list = [6, 3, 12, 7]
 # In the first pass of the algorithm, elements i and j are compared, placing the smaller one in the first position #
 list = [3, 6, 12, 7]
+# In the second pass, the algorithm compares 6 and 12 and does not swap them, since 6 < 12 #
+list = [3, 6, 12, 7]
+# In the third pass, the algorithm compares 12 and 7 and swaps them, because 7 < 12 #
+list = [3, 6, 7, 12]
+
