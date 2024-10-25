c9638033eb59b4eee99f7946810c5a9a0fbea288
Isabela Iunes <iunesisabela.cs@gmail.com>
2024-10-25 09:31:19 -0300

    master

--- BubbleSort.py
+++ BubbleSort.py
@@ -4,3 +4,8 @@
 # Conditional: If element i is greater than element j, swap elements i and j #
 # Display the sorted array #
 # End #
+
+# Consider this list as an example for sorting with Bubble Sort #
+list = [6, 3, 12, 7]
+# In the first pass of the algorithm, elements i and j are compared, placing the smaller one in the first position #
+list = [3, 6, 12, 7]
