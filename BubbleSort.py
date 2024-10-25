63f9799282f6d2f0966add0c1e17ee696cb84fd0
Isabela Iunes <iunesisabela.cs@gmail.com>
2024-10-25 09:30:22 -0300

    master

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,3 +1,6 @@
 # Start #
 # Loop1: For each element i in the array of size n #
 # Loop2: For each element j in the array of size n-1 (compare the elements again) #
+# Conditional: If element i is greater than element j, swap elements i and j #
+# Display the sorted array #
+# End #
